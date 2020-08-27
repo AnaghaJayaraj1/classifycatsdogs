a3960505c595d596e4f22a4938abc22875980e49
Anagha Jayaraj <anaghajayaraj01@gmail.com>
2020-08-27 12:14:15 +0530

    Initial commit

---
+++ knn.py
@@ -0,0 +1,75 @@
+# import the necessary packages
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from imutils import paths
+from PIL import Image
+#from tqdm import tqdm
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+
+
+def extract_color_histogram(image, bins=(8, 8, 8)):
+	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+	hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
+		[0, 180, 0, 256, 0, 256])
+ 
+	if imutils.is_cv2():
+		hist = cv2.normalize(hist)
+	else:
+		cv2.normalize(hist, hist)
+ 
+	return hist.flatten()
+
+
+ 
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-k", "--neighbors", type=int, default=1,
+	help="# of nearest neighbors for classification")
+ap.add_argument("-j", "--jobs", type=int, default=-1,
+	help="# of jobs for k-NN distance (-1 uses all available cores)")
+args = vars(ap.parse_args())
+
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+features = []
+labels = []
+ 
+for (i, imagePath) in enumerate(imagePaths):
+	image = cv2.imread(imagePath)
+	label = imagePath.split(os.path.sep)[-1].split(".")[0]
+ 
+	hist = extract_color_histogram(image)
+ 
+	features.append(hist)
+	labels.append(label)
+ 
+	if i > 0 and i % 1000 == 0:
+		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
+ 
+features = np.array(features)
+labels = np.array(labels)
+
+print("[INFO] features matrix: {:.2f}MB".format(
+	features.nbytes / (1024 * 1000.0)))
+
+(trainFeat, testFeat, trainLabels, testLabels) = train_test_split(features, labels, test_size=0.25, random_state=42)
+ 
+print("[INFO] evaluating histogram accuracy...")
+model = KNeighborsClassifier(n_neighbors=args["neighbors"],
+	n_jobs=args["jobs"])
+model.fit(trainFeat, trainLabels)
+acc = model.score(testFeat, testLabels)
+print("[INFO] histogram accuracy: {:.2f}%".format(acc * 100))
+
+
+
+img = cv2.imread("ruby.jpeg")
+h = extract_color_histogram(img)
+prediction = model.predict([h])
+print(prediction)
